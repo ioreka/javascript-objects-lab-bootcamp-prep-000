177032c4a7abf7592b10f1df61380eb6b11ac109
Lucy Mitchell <lucymhodges@gmail.com>
2018-04-25 21:47:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, recipes, { [key]: value })
 }
+
+
+
+
+   function() {
+      var obj = { prop: 1 }
+
+      expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+
+    it('it does not modify the original object, but rather returns a clone with the new data', function() {
+      var obj = { prop: 1 }
+
+      updateObjectWithKeyAndValue(obj, 'prop2', 2)
+
+      expect(obj['prop2']).toBe(undefined)
+    })
+  })
